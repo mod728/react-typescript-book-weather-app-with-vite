26934856dd3abd024bda7945d7c514ccc107d26b
mod728 <phallus-phaser@hotmail.com>
2024-04-02 15:59:30 +0900

    Code for Chapter 2

---
+++ src/components/Form.tsx
@@ -0,0 +1,21 @@
+type FormProps = {
+    setCity: React.Dispatch<React.SetStateAction<string>>
+    getWeather: (e: React.FormEvent<HTMLFormElement>) => void
+}  
+
+const Form = (props: FormProps) => {
+    return (
+        <form onSubmit={props.getWeather}>
+            <input type="text" 
+                   name="city" 
+                   placeholder="都市名"
+                   onChange={e => props.setCity(e.target.value)} 
+            />
+            <button type="submit">
+                Get Weather
+            </button>
+        </form>
+    )
+}
+
+export default Form
